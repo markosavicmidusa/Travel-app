ce1be877e53b83dbd28941d4d3e45e80e963e213
Marko Savic <marko@minorityprogrammers.org>
2022-02-14 18:00:35 +0100

    Initialization of components

--- travel_app/src/app/app.module.ts
+++ travel_app/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TripsComponent } from './trips/trips.component';
+
+import { ProfileComponent } from './profile/profile.component';
+import { MyTripsComponent } from './profile/my-trips/my-trips.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignupComponent,
+    LoginComponent,
+    TripsComponent,
+    ProfileComponent,
+    MyTripsComponent
   ],
   imports: [
     BrowserModule
