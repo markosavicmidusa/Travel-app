8774e1fa16cc1227409c0a5b7b9d13ea3f6e4c8a
Marko Savic <marko@minorityprogrammers.org>
2022-02-14 20:44:43 +0100

    adding material-modules

--- travel_app/src/app/material.module.ts
+++ travel_app/src/app/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
