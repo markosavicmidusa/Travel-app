942f650e30e1ee6dd07c976a615f59e6600169e1
Marko Savic <marko@minorityprogrammers.org>
2022-02-14 18:53:17 +0100

    Initialisation of routing module, with all paths and components

---
+++ travel_app/src/app/routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TripsComponent } from './trips/trips.component';
+
+const rute: Routes = [
+  {path: "", component: WelcomeComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "login", component: LoginComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "trips", component: TripsComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(rute)
+    
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class RoutingModule { }
