dbe63f50e5e38cf5baac59fe5f60458d314e2b79
Marko Savic <marko@minorityprogrammers.org>
2022-02-15 18:48:22 +0100

    footer adding

--- travel_app/src/app/material.module.ts
+++ travel_app/src/app/material.module.ts
@@ -5,7 +5,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [],
@@ -15,14 +15,17 @@ import {MatButtonModule} from '@angular/material/button';
     MatListModule,
     MatIconModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
+    
   ],
   exports: [
     MatSidenavModule,
     MatListModule,
     MatIconModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
