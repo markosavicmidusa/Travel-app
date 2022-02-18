da07eea35ab60feb3db04702134e85e25378f912
Marko Savic <marko@minorityprogrammers.org>
2022-02-18 22:04:54 +0100

    trips component finishing

--- travel_app/src/app/trips/trips.component.ts
+++ travel_app/src/app/trips/trips.component.ts
@@ -1,21 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddToCardComponent } from './add-to-card/add-to-card.component';
+import { TripsServiceService } from './trips-service.service';
 
-export interface TripsModel {
 
-  id: number,
-  destination: string,
-  picture_path: string,
-  travel_type: string,
-  duration: number,
-  distance: number,
-  price: number,
-  description: string,
-  time: Date,
-  recensents: Array<string>,
-  recensions: Array<string>,
-  grade: Array<number>,
-  status: string
-}
 export interface TravelType {
   value: string;
   viewValue: string;
@@ -32,73 +20,66 @@ export interface PricesRange {
   templateUrl: './trips.component.html',
   styleUrls: ['./trips.component.css']
 })
+
+
+
 export class TripsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public TravelData: TripsServiceService, private dialog: MatDialog) { }
 
-  
 
   public travels: TravelType[] = [
 
-    { value: 'airplane', viewValue: "Airplane"},
-    { value: 'train', viewValue: "Train"},
-    { value: 'bus', viewValue: "Bus"}
+    { value: 'airplane', viewValue: "Airplane" },
+    { value: 'train', viewValue: "Train" },
+    { value: 'bus', viewValue: "Bus" }
 
   ]
 
   public prices: PricesRange[] = [
 
-    {value: 1000.00, viewValue1: 0.00, viewValue2: 1000.00},
-    {value: 2000.00, viewValue1: 1000.00, viewValue2: 2000.00},
-    {value: 3000.00, viewValue1: 2000.00, viewValue2: 3000.00},
-    {value: 4000.00, viewValue1: 3000.00, viewValue2: 4000.00},
-    {value: 5000.00, viewValue1: 4000.00, viewValue2: 5000.00},
-    {value: 6000.00, viewValue1: 5000.00, viewValue2: 6000.00}
+    { value: 1000.00, viewValue1: 0.00, viewValue2: 1000.00 },
+    { value: 2000.00, viewValue1: 1000.00, viewValue2: 2000.00 },
+    { value: 3000.00, viewValue1: 2000.00, viewValue2: 3000.00 },
+    { value: 4000.00, viewValue1: 3000.00, viewValue2: 4000.00 },
+    { value: 5000.00, viewValue1: 4000.00, viewValue2: 5000.00 },
+    { value: 6000.00, viewValue1: 5000.00, viewValue2: 6000.00 }
   ]
 
 
-  static dummyTravelData: TripsModel[] = [
 
-    {
-      id: 1,
-      destination: 'Dubai',
-      picture_path: '../../assets/images/dubai.png',
-      travel_type: 'plane',
-      duration: 10,
-      distance: 5105,
-      price: 2000.00,
-      description: 'Dubai is the strongest and most densely populated city in the United Arab Emirates. A rest period of ten days is quite enough to take a break.You will enjoy a five- stars hotel with beautiful views of the Burj Khalifa.Dubai has many beautiful beaches that you must visit.',
-      time: new Date('2022-06-06T01:01'),
-      recensents: ['Tina', 'Jovan', 'Mario', 'Nina', 'Marina'],
-      recensions: ['Great journey.', 'Great all recommendations!', 'Solid.', 'Nothing special.', 'Fantastic'],
-      grade: [7, 10, 5, 2, 9],
-      status: 'active'
-    },
-    {
-      id: 2,
-      destination: 'Dubai',
-      picture_path: '../../assets/images/dubai.png',
-      travel_type: 'plane',
-      duration: 10,
-      distance: 5105,
-      price: 2000.00,
-      description: 'Dubai is the strongest and most densely populated city in the United Arab Emirates. A rest period of ten days is quite enough to take a break.You will enjoy a five- stars hotel with beautiful views of the Burj Khalifa.Dubai has many beautiful beaches that you must visit.',
-      time: new Date('2022-06-06T01:01'),
-      recensents: ['Tina', 'Jovan', 'Mario', 'Nina', 'Marina'],
-      recensions: ['Great journey.', 'Great all recommendations!', 'Solid.', 'Nothing special.', 'Fantastic'],
-      grade: [7, 10, 5, 2, 9],
-      status: 'active'
-    }
+  ngOnInit(): void {
 
-  ]
+  }
 
-  public currentTravel: TripsModel = TripsComponent.dummyTravelData[0];
-  
+  addToCardDialog() {
 
-ngOnInit(): void {
-  
-  console.log(this.currentTravel)
-  
-}
+
+    const dialogRef = this.dialog.open(AddToCardComponent, {
+      data: {
+        id: this.TravelData.currentTravel.id,
+        destination: this.TravelData.currentTravel.destination,
+        picture_path: this.TravelData.currentTravel.picture_path,
+        travel_type: this.TravelData.currentTravel.travel_type,
+        duration: this.TravelData.currentTravel.duration,
+        distance: this.TravelData.currentTravel.distance,
+        price: this.TravelData.currentTravel.price,
+        description: this.TravelData.currentTravel.description,
+        time: this.TravelData.currentTravel.time,
+        recensents: this.TravelData.currentTravel.recensents,
+        recensions: this.TravelData.currentTravel.recensions,
+        grade: this.TravelData.currentTravel.grade,
+        status: this.TravelData.currentTravel.status
+      }
+    });
+
+
+  }
+ 
+
+
+
+
 
 }
+
