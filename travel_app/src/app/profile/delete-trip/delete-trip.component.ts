67c33c6ed4dc69c6d34d6ba202c97e7f8b632824
Marko Savic <marko@minorityprogrammers.org>
2022-02-20 18:51:16 +0100

    delete trip component

---
+++ travel_app/src/app/profile/delete-trip/delete-trip.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-trip',
+  templateUrl: './delete-trip.component.html',
+  styleUrls: ['./delete-trip.component.css']
+})
+export class DeleteTripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
