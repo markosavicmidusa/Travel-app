de1d7726b26a1f3f6d6b42fcce427ea32879ff7a
Marko Savic <marko@minorityprogrammers.org>
2022-02-20 18:50:18 +0100

    rate trip component

---
+++ travel_app/src/app/profile/finished-trips/rate-trip/rate-trip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-trip',
+  templateUrl: './rate-trip.component.html',
+  styleUrls: ['./rate-trip.component.css']
+})
+export class RateTripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
